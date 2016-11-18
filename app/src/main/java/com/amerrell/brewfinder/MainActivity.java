c2d1a9e95b191d053dee9fe403c408e28e6c0b9c
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-11-18 09:01:58 -0800

    add button to Main Activity to transition to new Brews Activity.

--- app/src/main/java/com/amerrell/brewfinder/MainActivity.java
+++ app/src/main/java/com/amerrell/brewfinder/MainActivity.java
@@ -1,13 +1,31 @@
 package com.amerrell.brewfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.viewBrewsButton) Button mViewBrewsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mViewBrewsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mViewBrewsButton) {
+            Intent intent = new Intent(MainActivity.this, BrewsActivity.class);
+            startActivity(intent);
+        }
     }
 }
