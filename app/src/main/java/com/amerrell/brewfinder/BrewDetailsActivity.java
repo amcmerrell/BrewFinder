4a90957cf5f0c9c513f7e4527814c0c38835d00d
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-11-18 10:08:38 -0800

    create empty BrewDetailsActivity.

---
+++ app/src/main/java/com/amerrell/brewfinder/BrewDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.amerrell.brewfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BrewDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brew_details);
+    }
+}
