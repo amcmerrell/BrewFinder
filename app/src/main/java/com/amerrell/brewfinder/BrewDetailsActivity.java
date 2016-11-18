fef08056c0594ab5c1ea8ea4678896c6593fff9e
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-11-18 10:57:26 -0800

    add backend logic to validate and then display submitted note on BrewDetailsActivity.

--- app/src/main/java/com/amerrell/brewfinder/BrewDetailsActivity.java
+++ app/src/main/java/com/amerrell/brewfinder/BrewDetailsActivity.java
@@ -3,14 +3,21 @@ package com.amerrell.brewfinder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class BrewDetailsActivity extends AppCompatActivity {
+public class BrewDetailsActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.beerTitleView) TextView mBeerTitleView;
     @Bind(R.id.beerDescriptionView) TextView mBeerDescriptionView;
+    @Bind(R.id.noteEditText) EditText mNoteEditText;
+    @Bind(R.id.beerNotesView) TextView mBeerNotesView;
+    @Bind(R.id.addNoteButton) Button mAddNoteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,20 @@ public class BrewDetailsActivity extends AppCompatActivity {
 
         mBeerTitleView.setText(beerName);
         mBeerDescriptionView.setText(beerDescription);
+
+        mAddNoteButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mAddNoteButton) {
+            String note = mNoteEditText.getText().toString();
+            if (note.length() > 0) {
+                mBeerNotesView.append(note);
+                mNoteEditText.setText("");
+            } else {
+                Toast.makeText(BrewDetailsActivity.this, "Please add a note before submitting.", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
