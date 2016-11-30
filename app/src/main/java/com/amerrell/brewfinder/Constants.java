9c9bf9cc079fb2d15ed10af451e8a76b681129b0
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-11-30 14:05:48 -0800

    add reference to BreweryDB credentials to Constants file and update build.gradle to include this value on build.

---
+++ app/src/main/java/com/amerrell/brewfinder/Constants.java
@@ -0,0 +1,8 @@
+package com.amerrell.brewfinder;
+
+/**
+ * Created by Andrew on 11/30/2016.
+ */
+public class Constants {
+    public static final String BREWERYDB_API_KEY = BuildConfig.BREWERYDB_API_KEY;
+}
