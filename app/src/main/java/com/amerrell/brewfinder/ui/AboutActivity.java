a8e904e2e0043e969e02b075da3958e9fcfd8509
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-02 16:59:40 -0800

    add implicit intent to about activity.

--- app/src/main/java/com/amerrell/brewfinder/ui/AboutActivity.java
+++ app/src/main/java/com/amerrell/brewfinder/ui/AboutActivity.java
@@ -1,8 +1,10 @@
 package com.amerrell.brewfinder.ui;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.amerrell.brewfinder.R;
@@ -10,9 +12,10 @@ import com.amerrell.brewfinder.R;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.aboutTitleTextView) TextView mAboutTitleTextView;
     @Bind(R.id.contactTitleTextView) TextView mContactTitleTextView;
+    @Bind(R.id.contactEmailTextView) TextView mContactEmailTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,15 @@ public class AboutActivity extends AppCompatActivity {
         Typeface goodDog = Typeface.createFromAsset(getAssets(), "fonts/GoodDog.ttf");
         mAboutTitleTextView.setTypeface(goodDog);
         mContactTitleTextView.setTypeface(goodDog);
+        mContactEmailTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mContactTitleTextView) {
+            Intent emailIntent = new Intent(Intent.ACTION_MAIN);
+            emailIntent.addCategory(Intent.CATEGORY_APP_EMAIL);
+            startActivity(emailIntent);
+        }
     }
 }
