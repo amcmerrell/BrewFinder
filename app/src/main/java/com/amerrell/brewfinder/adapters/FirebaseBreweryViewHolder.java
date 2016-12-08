05ebfd41963d11e8632d85ed32109e3dbaf6a915
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-08 12:23:20 -0800

    add firebase adapter and create layout to display saved breweries.

---
+++ app/src/main/java/com/amerrell/brewfinder/adapters/FirebaseBreweryViewHolder.java
@@ -0,0 +1,35 @@
+package com.amerrell.brewfinder.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.amerrell.brewfinder.R;
+import com.amerrell.brewfinder.models.Brewery;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Created by Guest on 12/8/16.
+ */
+public class FirebaseBreweryViewHolder extends RecyclerView.ViewHolder {
+    View mView;
+    Context mContext;
+
+    public FirebaseBreweryViewHolder (View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+    }
+
+    public void bindBrewery(Brewery brewery) {
+        ImageView savedBreweryImageView = (ImageView) mView.findViewById(R.id.savedBreweryListImageView);
+        TextView savedBreweryTitleTextView = (TextView) mView.findViewById(R.id.savedBreweryNameTextView);
+        TextView savedBreweryAddressTextView = (TextView) mView.findViewById(R.id.savedAddressTextView);
+
+        Picasso.with(mContext).load(brewery.getLogoUrl()).into(savedBreweryImageView);
+        savedBreweryTitleTextView.setText(brewery.getName());
+        savedBreweryAddressTextView.setText(brewery.getAddress());
+    }
+}
