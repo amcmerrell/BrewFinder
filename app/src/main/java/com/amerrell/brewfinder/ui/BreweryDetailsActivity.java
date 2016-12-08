c3ec5d7b32c52472897bdb66e53451bd2aa896ff
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-08 10:19:01 -0800

    add ability to save breweries to firebase.

--- app/src/main/java/com/amerrell/brewfinder/ui/BreweryDetailsActivity.java
+++ app/src/main/java/com/amerrell/brewfinder/ui/BreweryDetailsActivity.java
@@ -3,11 +3,17 @@ package com.amerrell.brewfinder.ui;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.amerrell.brewfinder.Constants;
 import com.amerrell.brewfinder.R;
 import com.amerrell.brewfinder.models.Brewery;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -17,11 +23,13 @@ import java.util.ArrayList;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class BreweryDetailsActivity extends AppCompatActivity {
+public class BreweryDetailsActivity extends AppCompatActivity implements View.OnClickListener{
     @Bind(R.id.breweryDetailsTitleTextView) TextView mBreweryTitleTextView;
     @Bind(R.id.breweryAddressTextView) TextView mBreweryAddressTextView;
     @Bind(R.id.breweryDetailsImageView) ImageView mBreweryDetailsImageView;
     @Bind(R.id.breweryPhoneTextView) TextView mBreweryPhoneTextView;
+    @Bind(R.id.saveBreweryButton) Button mSaveBreweryButton;
+
     private ArrayList<Brewery> mBreweries;
     private Brewery mBrewery;
     private int mPosition;
@@ -43,5 +51,18 @@ public class BreweryDetailsActivity extends AppCompatActivity {
         mBreweryAddressTextView.setText(mBrewery.getAddress());
         mBreweryPhoneTextView.setText(mBrewery.getPhone());
         Picasso.with(this).load(mBrewery.getLogoUrl()).into(mBreweryDetailsImageView);
+
+        mSaveBreweryButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mSaveBreweryButton) {
+            DatabaseReference breweryRef = FirebaseDatabase
+                    .getInstance()
+                    .getReference(Constants.FIREBASE_CHILD_BREWERIES);
+            breweryRef.push().setValue(mBrewery);
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
