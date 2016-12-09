4b5ec56144f669a986cbfcdb80a9d471908a74f1
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-09 15:28:51 -0800

    add overflow menu to main activity and logout button.

--- app/src/main/java/com/amerrell/brewfinder/ui/MainActivity.java
+++ app/src/main/java/com/amerrell/brewfinder/ui/MainActivity.java
@@ -3,9 +3,12 @@ package com.amerrell.brewfinder.ui;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +16,7 @@ import android.widget.TextView;
 
 import com.amerrell.brewfinder.Constants;
 import com.amerrell.brewfinder.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -43,6 +47,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSavedBreweriesButton.setOnClickListener(this);
     }
 
+    private void addToSharedPreferences(String zipCode) {
+        mEditor.putString(Constants.PREFERENCES_ZIPCODE_KEY, zipCode).apply();
+    }
+
     @Override
     public void onClick(View v) {
         if (v == mViewBreweriesButton) {
@@ -61,7 +69,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    private void addToSharedPreferences(String zipCode) {
-        mEditor.putString(Constants.PREFERENCES_ZIPCODE_KEY, zipCode).apply();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
