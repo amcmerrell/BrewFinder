d093cf5e802d25bcc160ae143256063faf33600c
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-09 15:06:12 -0800

    add ability to create user and store in firebase.

--- app/src/main/java/com/amerrell/brewfinder/ui/LoginActivity.java
+++ app/src/main/java/com/amerrell/brewfinder/ui/LoginActivity.java
@@ -1,15 +1,39 @@
 package com.amerrell.brewfinder.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.amerrell.brewfinder.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @Bind(R.id.passwordLoginButton) Button mLoginButton;
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mRegisterTextView.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mLoginButton) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        } else if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
