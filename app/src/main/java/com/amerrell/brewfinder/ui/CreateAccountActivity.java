1876baa65b6622d831c576133d6e06175db64653
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-09 13:41:27 -0800

    add Create Account and Login activities along with styled layout files.

---
+++ app/src/main/java/com/amerrell/brewfinder/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.amerrell.brewfinder.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amerrell.brewfinder.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
